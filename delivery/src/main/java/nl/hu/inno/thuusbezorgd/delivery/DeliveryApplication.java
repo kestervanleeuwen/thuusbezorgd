b7412be6855091b46e11f1470c5978dbbafe6beb
Kester van Leeuwen <kester@posonas.com>
2023-02-04 17:15:20 +0100

    init db

--- delivery/src/main/java/nl/hu/inno/thuusbezorgd/delivery/DeliveryApplication.java
+++ delivery/src/main/java/nl/hu/inno/thuusbezorgd/delivery/DeliveryApplication.java
@@ -4,9 +4,10 @@ package nl.hu.inno.thuusbezorgd.delivery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DeliveryApplication {
     public static void main(String[] args) {
-        SpringApplication.run(nl.hu.inno.thuusbezorgd.order.OrdersApplication.class, args);
+        SpringApplication.run(DeliveryApplication.class, args);
     }
 }
